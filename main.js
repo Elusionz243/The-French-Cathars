1a772801a995efc13c5fb51867b4b8e9d51213a4
Elusionz243 <strivelinkband@yahoo.com>
2022-03-23 03:03:24 +0600

    Initial Commit

---
+++ main.js
@@ -0,0 +1,59 @@
+document.getElementById("whobtn").addEventListener("click", fullscreenWho);
+document.getElementById("whatbtn").addEventListener("click", fullscreenWhat);
+document.getElementById("whybtn").addEventListener("click", fullscreenWhy);
+document
+  .getElementById("learn-morebtn")
+  .addEventListener("click", fullscreenLearnMore);
+document.getElementById("cathar-cross").addEventListener("click", openVideo);
+
+function fullscreenWho() {
+  let content = document.getElementById("who-content").innerHTML;
+  let title = document.getElementById("who-title").innerText;
+  document.querySelector(".overlay-content").innerHTML = content;
+  document.querySelector(".overlay-title").innerText = title;
+  document.getElementById("Fullscreen").style.width = "100%";
+  document.querySelector(".overlay-content").classList.add("scrollbar");
+}
+
+function fullscreenWhat() {
+  let content = document.getElementById("what-content").innerHTML;
+  let title = document.getElementById("what-title").innerText;
+  document.querySelector(".overlay-content").innerHTML = content;
+  document.querySelector(".overlay-title").innerText = title;
+  document.getElementById("Fullscreen").style.width = "100%";
+  document.querySelector(".overlay-content").classList.add("scrollbar");
+}
+
+function fullscreenWhy() {
+  let content = document.getElementById("why-content").innerHTML;
+  let title = document.getElementById("why-title").innerText;
+  document.querySelector(".overlay-content").innerHTML = content;
+  document.querySelector(".overlay-title").innerText = title;
+  document.getElementById("Fullscreen").style.width = "100%";
+  document.querySelector(".overlay-content").classList.add("scrollbar");
+}
+
+function fullscreenLearnMore() {
+  let content = document.getElementById("learn-more-content").innerHTML;
+  let title = document.getElementById("learn-more-title").innerText;
+  document.querySelector(".overlay-content").innerHTML = content;
+  document.querySelector(".overlay-title").innerText = title;
+  document.getElementById("Fullscreen").style.width = "100%";
+  document.querySelector(".overlay-content").classList.add("scrollbar");
+}
+
+function openVideo() {
+  let content = document.getElementById("video").innerHTML;
+  document.querySelector(".overlay-content").innerHTML = content;
+  document.getElementById("Fullscreen").style.width = "100%";
+}
+
+function closeFullscreen() {
+  if (document.getElementById("video").style.width === "100%") {
+    document.getElementsByTagName("video").pause();
+    document.getElementById("video").style.width = "0%";
+  }
+  document.querySelector(".overlay-content").innerHTML = "";
+  document.querySelector(".overlay-title").innerText = "";
+  document.getElementById("Fullscreen").style.width = "0%";
+}
